c313459bbda6835c4b089c9f9a035e0fc73c1532
Replit user <>
2023-08-26 02:45:18 +0000

    new try

--- list.c
+++ list.c
@@ -19,7 +19,7 @@ struct List {
 typedef struct List List;
 
 Node *createNode(void *data) {
-  Node *new = (Node *)malloc(sizeof(Node));
+  Node *new = (Node *)callloc(sizeof(Node));
   assert(new != NULL);
   new->data = data;
   new->prev = NULL;
